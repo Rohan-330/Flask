ab59812611db3bd87cf2dfc828b2efd3cc7f4f13
Rohan Kanaskar <kanaskarrohan330@gmail.com>
2026-04-29 14:51:00 +0530

    Implement Flask app to check leap years

--- Leap-year-app/app.py
+++ Leap-year-app/app.py
@@ -1,1 +1,18 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Use URL: /leap/year"
+
+@app.route('/leap/<int:year>')
+def leap(year):
+    if year % 4 == 0:
+        return str(year) + " is a Leap Year"
+    else:
+        return str(year) + " is NOT a Leap Year"
+
+
+if __name__ == '__main__':
+    app.run(debug=True) 
